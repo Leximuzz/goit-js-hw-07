c1125ed5cea02356ad91d3c53d133c43fcdb3fa5
Oleksandr <114010794+Leximuzz@users.noreply.github.com>
2024-05-24 06:50:48 +0300

    task-3

--- js/task-3.js
+++ js/task-3.js
@@ -1,3 +1,8 @@
+// const form = document.querySelector(".js-form");
+// form.addEventListener("submit", (e) => {
+//   e.preventDefault();
+// });
+
 const inputElem = document.querySelector("#name-input");
 inputElem.addEventListener("input", userNameInput);
 const outputElem = document.getElementById("name-output");
